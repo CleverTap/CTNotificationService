67a05dac59a0e2bf373ab8974cb7a4c970921328
Peter Wilkniss <peter@iMac.local>
2017-07-26 13:41:01 -0700

    clean up example

--- ExampleSwift/NotificationService/NotificationService.swift
+++ ExampleSwift/NotificationService/NotificationService.swift
@@ -7,9 +7,6 @@ class NotificationService: CTNotificationServiceExtension {
     var bestAttemptContent: UNMutableNotificationContent?
 
     override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
-        self.mediaUrlKey = "myMediaUrlKey"
-        self.mediaTypeKey = "myMediaTypeKey"
-        
         super.didReceive(request, withContentHandler: contentHandler)
     }
 }
