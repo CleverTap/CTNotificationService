097fbea3ba845fde69bae83fae0f45f8c1bf9238
Aditi3 <Aditi3@users.noreply.github.com>
2021-12-01 10:31:15 +0530

    Update to ExampleSwiftPM

--- CTNotificationService/CTNotificationService.h
+++ CTNotificationService/CTNotificationService.h
@@ -5,8 +5,4 @@ extern double CTNotificationServiceVersionNumber;
 //! Project version string for CTNotificationService.
 extern const unsigned char CTNotificationServiceVersionString[];
 
-#if SWIFT_PACKAGE
 #import "CTNotificationServiceExtension.h"
-#else
-#import <CTNotificationService/CTNotificationServiceExtension.h>
-#endif
