f42c719d9717067d1f70d1ab00fdf6e850b38bbd
Aditi3 <Aditi3@users.noreply.github.com>
2021-12-02 07:19:40 +0530

    Enable PNs for ExampleSwiftPM

--- ExampleSwiftPM/CTNotificationServiceExampleSPM/AppDelegate.swift
+++ ExampleSwiftPM/CTNotificationServiceExampleSPM/AppDelegate.swift
@@ -6,16 +6,41 @@
 //
 
 import UIKit
+import CleverTapSDK
+import UserNotifications
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
     
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        CleverTap.autoIntegrate()
+        CleverTap.setDebugLevel(3)
+        registerPush()
         return true
     }
     
+    private func registerPush() {
+        // request permissions
+        if #available(iOS 10.0, *) {
+            UNUserNotificationCenter.current().delegate = self
+            UNUserNotificationCenter.current().requestAuthorization(options: [.sound, .alert, .badge]) {
+                (granted, error) in
+                if (granted) {
+                    DispatchQueue.main.async {
+                        UIApplication.shared.registerForRemoteNotifications()
+                    }
+                }
+            }
+        } else {
+            // Fallback on earlier versions
+        }
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        print("did receive remote notification \(response.notification.request.content.userInfo)")
+    }
 }
 
