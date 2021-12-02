4c94f007a21192e500619d7595de749041dc5d08
Aditi3 <Aditi3@users.noreply.github.com>
2021-12-02 11:04:58 +0530

    Code Cleanup

--- ExampleSwiftPM/CTNotificationServiceExampleSPM/AppDelegate.swift
+++ ExampleSwiftPM/CTNotificationServiceExampleSPM/AppDelegate.swift
@@ -17,7 +17,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         CleverTap.autoIntegrate()
-        CleverTap.setDebugLevel(3)
         registerPush()
         return true
     }
